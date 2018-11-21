44809ef0e37fb159f5d65d776e196c20ee12ad70
Nate Lawson <44374803+nlawson28@users.noreply.github.com>
2018-11-21 14:27:06 -0500

    Lab 7 updates

---
+++ Lab 7/src/Product.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Product {
+
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String productName;
+        double total = 0, average, productCost;
+        int count = 0;
+
+        System.out.println("Products that cost $100.00 or more");
+        System.out.println("Enter the product ordered - type 'stop' to end: ");
+        productName = input.nextLine();
+        while (!productName.equalsIgnoreCase("stop")) {
+            System.out.println("Enter the cost of the product ordered: ");
+            productCost = input.nextDouble();
+            input.nextLine();
+            if (productCost >= 100) {
+                count = count + 1;
+                total = total + productCost;
+            }
+            System.out.println("Enter the product ordered - type 'stop' to end: ");
+            productName = input.nextLine();
+        }
+
+        System.out.println("There were " + count + " item(s) that had a cost of $100.00 or more");
+        average = total / count;
+
+        //This equation below allows for the amount to be round to the nearest 2 decimals places to avoid run on amounts
+        average = Math.round(average*100.0)/100.0;
+
+        System.out.println("The average price of items with a cost of $100.00 or more is $" + average);
+        System.out.println("End of report");
+    }
+}
